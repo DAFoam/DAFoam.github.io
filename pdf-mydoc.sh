8a7a8d340c9d5c4d3c71578003adceff249b6e01
Ping He <phe@iastate.edu>
2021-06-10 09:39:02 -0500

    content update

--- pdf-mydoc.sh
+++ pdf-mydoc.sh
@@ -9,6 +9,6 @@ bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_mydoc_p
 echo "done";
 
 echo "Building the PDF ...";
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/mydoc.pdf;
+prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o mydoc.pdf;
 
-echo "Done. Look in the pdf directory to see if it printed successfully."
+echo "Done."
